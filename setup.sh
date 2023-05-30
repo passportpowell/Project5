82d66ba539e2344b99b163397fcbcb65427ef6e6
passportpowell <otisofro@gmail.com>
2023-05-30 18:06:54 +0000

    add files to gitignore to hopefully reduce space for herku

--- setup.sh
+++ setup.sh
@@ -1,23 +1,8 @@
-# mkdir -p ~/.streamlit/
-# echo "\
-# [server]\n\
-# headless = true\n\
-# port = $PORT\n\
-# enableCORS = false\n\
-# \n\
-# " > ~/.streamlit/config.toml
-
 mkdir -p ~/.streamlit/
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
-\n\
-[theme]\n\
-base='dark'\n\
-primaryColor='#586e46'\n\
-textColor='#bbb'\n\
-secondaryBackgroundColor='#101c2c'\n\
 \n\
 " > ~/.streamlit/config.toml
